1bad97a55e31d0ae7b984c3d8b6d1485b102503f
IT22280824 <it22280824@my.sliit.lk>
2025-08-26 11:06:24 +0530

    Basic SignUp page

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,17 @@
 <template>
-
+  <div id="app">
+    <SignUp />
+  </div>
 </template>
 
 <script>
-
+import SignUp from './components/SignUp.vue'
 
 export default {
   name: 'App',
+  components: {
+    SignUp
+  }
 }
 </script>
 
